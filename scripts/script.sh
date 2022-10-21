2783f7fa59a1154e4bd38dec8b46a02fc60e6407
Cici Hou <41445818+cicihou@users.noreply.github.com>
2022-10-21 16:13:19 -0700

    test pyenv

--- scripts/script.sh
+++ scripts/script.sh
@@ -16,25 +16,37 @@ sudo apt-get -y install mysql-server
 sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'mysql1234';"
 sudo mysql --user=root --password=mysql1234 -e "create database webapp charset = utf8mb4;"
 sudo service mysql restart
-echo "Install Python"
-cd /tmp
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev libgdbm-dev libnss3-dev libedit-dev libc6-dev
-wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz
-tar -xzf Python-3.6.15.tgz
-cd Python-3.6.15
-./configure --enable-optimizations --with-lto
-sudo make -j 1
-sudo make altinstall
-echo finish python install
-sudo apt install python3-pip -y
-cd ~
-python3.6 -m pip install -r requirements.txt
-python3.6 manage.py db stamp head
-python3.6 manage.py db migrate
-python3.6 manage.py db upgrade
+#echo "Install Python"
+#cd /tmp
+#sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev libgdbm-dev libnss3-dev libedit-dev libc6-dev
+#wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz
+#tar -xzf Python-3.6.15.tgz
+#cd Python-3.6.15
+#./configure --enable-optimizations --with-lto
+#sudo make -j 1
+#sudo make altinstall
+#echo finish python install
+#sudo apt install python3-pip -y
+#cd ~
+#python3.6 -m pip install -r requirements.txt
+#python3.6 manage.py db stamp head
+#python3.6 manage.py db migrate
+#python3.6 manage.py db upgrade
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install 3.6.15
+pyenv global 3.6.15
+pyenv virtualenv 3.6.15 webapp
+pyenv activate webapp
+pip install -r requirements.txt
+python manage.py db stamp head
+python manage.py db migrate
+python manage.py db upgrade
 echo "Install systemd"
 sudo apt-get -y install systemd
 sudo cp ~/scripts/python.service /lib/systemd/system/python.service
 sudo systemctl daemon-reload
-sudo systemctl enable python.service
-sudo systemctl start python.service
+sudo systemctl enable /lib/systemd/system/python.service
+sudo systemctl start /lib/systemd/system/python.service
