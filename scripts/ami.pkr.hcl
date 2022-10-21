115ac8fa3a8127ef1f21afadecb5b9ce56f04de2
Cici Hou <41445818+cicihou@users.noreply.github.com>
2022-10-21 10:47:25 -0700

    add: packer

---
+++ scripts/ami.pkr.hcl
@@ -0,0 +1,63 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "source_ami" {
+  type    = string
+  default = "ami-08c40ec9ead489470" # Ubuntu 22.04 LTS
+}
+
+variable "ssh_username" {
+  type    = string
+  default = "ubuntu"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-0aa1d1b8120b34fcd"
+}
+
+# https://www.packer.io/plugins/builders/amazon/ebs
+source "amazon-ebs" "my-ami" {
+  region     = "${var.aws_region}"
+  ami_name        = "csye6225_${formatdate("YYYY_MM_DD_hh_mm_ss", timestamp())}"
+  ami_description = "AMI for CSYE 6225"
+  ami_regions = [
+    "us-east-1",
+  ]
+  ami_users = [
+    "565610652211",
+    "291014418372",
+    "665648832676",
+  ]
+
+  aws_polling {
+    delay_seconds = 120
+    max_attempts  = 50
+  }
+
+  instance_type = "t2.micro"
+  source_ami    = "${var.source_ami}"
+  ssh_username  = "${var.ssh_username}"
+  subnet_id     = "${var.subnet_id}"
+
+  launch_block_device_mappings {
+    delete_on_termination = true
+    device_name           = "/dev/sda1"
+    volume_size           = 8
+    volume_type           = "gp2"
+  }
+}
+
+build {
+  sources = ["source.amazon-ebs.my-ami"]
+
+  provisioner "file" {
+    source = "./webapp.zip"
+    destination = "/home/ubuntu/"
+}
+  provisioner "shell" {
+    script = "script.sh"
+  }
+}
---
+++ scripts/script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+sudo apt-get install -y -q
+echo "Install Nginx"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo apt-get clean
+sudo apt-get install dialog apt-utils -y
+echo "Install Mysql"
+#sudo apt-get install debconf-utils
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysql1234'
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysql1234'
+sudo apt-get -y install mysql-server
+sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'mysql1234';"
+sudo mysql --user=root --password=mysql1234 -e "create database webapp charset = utf8mb4;"
+sudo service mysql restart
+echo "Install Python"
+sudo apt-get install -y python3.6
+#sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+#libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+#libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
+#libgdbm-dev libnss3-dev libedit-dev libc6-dev
+#wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz
+#tar -xzf Python-3.6.15.tgz
+#cd Python-3.6.15
+#./configure --enable-optimizations  -with-lto  --with-pydebug
+#make -j 8  # adjust for number of your CPU cores
+#sudo make altinstall
+#echo finish python install
+sudo apt-get install zip unzip -y
+cd ~
+unzip webapp.zip
+cd webapp
+pip install -r requirements.txt
+python manage.py db stamp head
+python manage.py db migrate
+python manage.py db upgrade
+echo "Install systemd"
+sudo apt-get -y install systemd
+sudo cp ~/webapp/scripts/python.service /lib/systemd/system/python.service
+sudo systemctl daemon-reload
+sudo systemctl enable python.service
+sudo systemctl start python.service
