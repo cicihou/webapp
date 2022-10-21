ac6914dd8596dd18d15e31c7bbf94f921c425886
Cici Hou <41445818+cicihou@users.noreply.github.com>
2022-10-21 14:34:59 -0700

    mod script

--- scripts/script.sh
+++ scripts/script.sh
@@ -17,25 +17,24 @@ sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha
 sudo mysql --user=root --password=mysql1234 -e "create database webapp charset = utf8mb4;"
 sudo service mysql restart
 echo "Install Python"
-#sudo apt-get install -y python3.6
-#sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-#libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-#libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
-#libgdbm-dev libnss3-dev libedit-dev libc6-dev
-#wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz
-#tar -xzf Python-3.6.15.tgz
-#cd Python-3.6.15
-#./configure --enable-optimizations  -with-lto  --with-pydebug
-#make -j 8  # adjust for number of your CPU cores
-#sudo make altinstall
-#echo finish python install
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev libgdbm-dev libnss3-dev libedit-dev libc6-dev
+wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz
+tar -xzf Python-3.6.15.tgz
+cd Python-3.6.15
+./configure
+make
+make test
+sudo make install
+echo finish python install
+sudo apt install python3-pip -y
+cd ~
 pip install -r requirements.txt
-python manage.py db stamp head
-python manage.py db migrate
-python manage.py db upgrade
+python3.6 manage.py db stamp head
+python3.6 manage.py db migrate
+python3.6 manage.py db upgrade
 echo "Install systemd"
 sudo apt-get -y install systemd
-sudo cp ~/webapp/scripts/python.service /lib/systemd/system/python.service
+sudo cp ~/scripts/python.service /lib/systemd/system/python.service
 sudo systemctl daemon-reload
 sudo systemctl enable python.service
 sudo systemctl start python.service
